23436614d8c86237c22b8a22ea5db95a562fc4c1
samuelleonelucas <samuel_leonel288@hotmail.com>
2019-09-11 02:03:12 -0300

    adicionando variaveis de ambiente

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.use('/files', express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use(require('./routes'));
 
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
 
 
 console.log('rodando na porta 3333')
